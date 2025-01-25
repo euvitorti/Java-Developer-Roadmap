1ac9bdd599a2060ebf2c44bdc65bd7abd62e5a82
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2025-01-25 14:41:11 -0300

    Abstract Classes and Methods

---
+++ Learn the Fundamentals/OOP/Abstraction/Animal.java
@@ -0,0 +1,10 @@
+package OOP.Abstraction;
+
+abstract class Animal {
+    // Abstract method (does not have a body)
+    public abstract void animalSound();
+    // Regular method
+    public void sleep() {
+        System.out.println("Zzz");
+    }
+}
---
+++ Learn the Fundamentals/OOP/Abstraction/Pig.java
@@ -0,0 +1,9 @@
+package OOP.Abstraction;
+
+// Subclass (inherit from Animal)
+class Pig extends Animal {
+    public void animalSound() {
+        // The body of animalSound() is provided here
+        System.out.println("The pig says: wee wee");
+    }
+}
